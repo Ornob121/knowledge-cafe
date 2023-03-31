33c93651460d28f6b8ec28cc9a19cccba7bdf153
Ornob121 <shadmantahmid121@gmail.com>
2023-03-31 11:20:30 +0600

    header done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between mx-auto mt-12 items-center font-semibold">
+      <h1 className="text-4xl">Knowledge Cafe</h1>
+      <img
+        className="h-14 w-14 rounded-full"
+        src="https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Header;
