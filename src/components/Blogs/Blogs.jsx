933c9c3f87e7b4f6a8347db593a59392fe06f070
Ornob121 <shadmantahmid121@gmail.com>
2023-03-31 15:31:06 +0600

    bookmark added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -13,8 +13,14 @@ const Blogs = () => {
       .then((data) => setBlogs(data));
   }, []);
   const setBookmark = (blog) => {
-    const newBookmark = [...bookmarked, blog];
-    setBookmarked(newBookmark);
+    const preBookmark = bookmarked.find((bm) => bm._id === blog._id);
+    console.log(preBookmark);
+    if (preBookmark) {
+      return alert("already bookmarked");
+    } else {
+      const newBookmark = [...bookmarked, blog];
+      setBookmarked(newBookmark);
+    }
   };
 
   const readTime = (time) => {
@@ -35,7 +41,7 @@ const Blogs = () => {
         ))}
       </div>
       <div className="ml-6">
-        <div className="text-center">
+        <div className="text-center  sticky top-0">
           <h4
             className="px-10  text-2xl font-bold py-5 rounded-lg"
             style={{ color: "#6047EC", background: "rgba(96, 71, 236, 0.1)" }}
@@ -43,12 +49,15 @@ const Blogs = () => {
             Spent time on read: {readingTime} min
           </h4>
         </div>
-        <div className="mt-6">
-          <h3 className="font-bold text-2xl">
-            Bookmarked Blogs :{bookmarked.length}
+        <div
+          className="mt-6 rounded-lg pb-4 sticky top-24"
+          style={{ background: "rgba(17, 17, 17, 0.05)" }}
+        >
+          <h3 className="font-bold text-2xl pt-7 pl-7 pb-4">
+            Bookmarked Blogs : {bookmarked.length}{" "}
           </h3>
           {bookmarked.map((bookmark) => (
-            <Bookmark bookmark={bookmark} key={bookmark.id}></Bookmark>
+            <Bookmark bookmark={bookmark} key={bookmark._id}></Bookmark>
           ))}
         </div>
       </div>
--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const Bookmark = ({ bookmark }) => {
-  console.log(bookmark);
-  return <div></div>;
+  return (
+    <div className="mx-4 drop-shadow mb-4 bg-white rounded-lg">
+      <h5 className="text-xl font-semibold px-5 py-5">{bookmark.title}</h5>
+    </div>
+  );
 };
 
 export default Bookmark;
